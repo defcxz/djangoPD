af4795b9bc9565dc583f97c1927de7794af448d3
defcxz <mario6632@gmail.com>
2019-09-28 16:21:17 -0300

    Initial commit

---
+++ webpersonal/principal/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+	return HttpResponse("<h1> Esta es mi página principal</h1>")
+
+def about(request):
+	return HttpResponse("<h1>Mi nombre es Macarena</h1>")	
