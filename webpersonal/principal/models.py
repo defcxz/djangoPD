b95c825679eed6009f2ab8664fa5ca0cb3bb1114
defcxz <mario6632@gmail.com>
2019-10-17 12:47:56 -0300

    clase 17-10

--- webpersonal/principal/models.py
+++ webpersonal/principal/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class ClassName(models.Model):
+		nombre = models.CharField(max_length = 255)
+		descripcion = models.CharField(max_length = 255)
